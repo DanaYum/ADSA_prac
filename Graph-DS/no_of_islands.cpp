1161fb8c4d491ad50d223db07d7b4f7aeb36370d
Dhana <dhanachandrayumnam12@gmail.com>
2025-06-30 10:20:54 +0530

    no of islands added

---
+++ Graph-DS/no_of_islands.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis, int delrow[], int delcol[]) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int count = 0;
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    count++;
+                    bfs(i, j, grid, vis, delrow, delcol);
+                }
+            }
+        }
+        return count;
+    }
+};
