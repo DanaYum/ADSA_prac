e6b6888bcc1132d043adbae5cb75be965ea94876
Dhana <dhanachandrayumnam12@gmail.com>
2025-07-16 11:44:40 +0530

    Shortest Path in Weighted undirected graph

--- Djisktra's_Algorithm/shortest_path.cpp
+++ Djisktra's_Algorithm/shortest_path.cpp
@@ -1,51 +1,87 @@
-//Shortest Path in Weighted undirected graph GFG
-
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
   public:
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
-        // Code here
         vector<pair<int,int>> adj[n+1];
         for(auto it: edges){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> dist(n+1,INT_MAX);
+
+        // Min-heap: (distance, node)
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(n+1, INT_MAX);
         vector<int> parent(n+1);
-        for(int i=1;i<=n;i++){
-            parent[i]=i;
-        }
-        dist[1]=0;
-        pq.push({1,0});
+
+        for(int i = 1; i <= n; i++) parent[i] = i;
+
+        dist[1] = 0;
+        pq.push({0, 1}); // (distance, node)
+
         while(!pq.empty()){
-            int node=pq.top().first;
-            int d=pq.top().second;
+            int d = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
-            for(auto it: adj[node]){
-                int adjNode=it.first;
-                int adjedge=it.second;
-                if(d + adjedge < dist[adjNode]){
-                    dist[adjNode]=d + adjedge;
-                    pq.push({adjNode,dist[adjNode]});
-                    parent[adjNode]=node;
+
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int weight = it.second;
+
+                if(d + weight < dist[adjNode]){
+                    dist[adjNode] = d + weight;
+                    pq.push({dist[adjNode], adjNode});
+                    parent[adjNode] = node;
                 }
             }
         }
-        if(dist[n]==INT_MAX){
+
+        // No path found
+        if(dist[n] == INT_MAX){
             return {-1};
         }
-        int node=n;
+
+        // Reconstruct the path
         vector<int> path;
-        while(parent[node]!=node){
+        int node = n;
+        while(parent[node] != node){
             path.push_back(node);
-            node=parent[node];
+            node = parent[node];
         }
-        path.push_back(1);
-        reverse(path.begin(),path.end());
-        path.insert(path.begin(), dist[n]);  // insert total weight at the beginning
+        path.push_back(1); // starting node
+        reverse(path.begin(), path.end());
+
+        // Insert total weight at the front
+        path.insert(path.begin(), dist[n]);
 
         return path;
     }
 };
+int main() {
+    int n = 5, m = 6;
+    vector<vector<int>> edges = {
+        {1, 2, 2},
+        {2, 5, 5},
+        {2, 3, 4},
+        {1, 4, 1},
+        {4, 3, 3},
+        {3, 5, 1}
+    };
+
+    Solution obj;
+    vector<int> result = obj.shortestPath(n, m, edges);
+
+    if(result.size() == 1 && result[0] == -1){
+        cout << "No path exists from 1 to " << n << endl;
+    } else {
+        cout << "Shortest Distance: " << result[0] << endl;
+        cout << "Path: ";
+        for(int i = 1; i < result.size(); i++){
+            cout << result[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
