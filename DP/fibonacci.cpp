fa8b2f0eb800a70e20ec48eb2874d3fc9ccf4929
Dhana <dhanachandrayumnam12@gmail.com>
2025-08-14 00:31:35 +0530

    dp on fibonacci

--- DP/fibonacci.cpp
+++ DP/fibonacci.cpp
@@ -11,16 +11,33 @@ using namespace std;
 //     cin>>n;
 //     vector<int> dp(n+1,-1);
 //     cout<<fib(n,dp);
-// }
+// } TC-> O(n) SC-> O(n)+O(n)
+
+
+
 // Fibonacci using tabulation
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<int> dp(n+1);
+//     dp[0]=0;
+//     dp[1]=1;
+//     for(int i=2;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+//     cout<<dp[n];
+// } TC-> O(n) SC-> O(n)
+
+
+// space optimization
 int main(){
     int n;
     cin>>n;
-    vector<int> dp(n+1);
-    dp[0]=0;
-    dp[1]=1;
+    int prev2=0,prev1=1;
     for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+        int curr=prev1 + prev2;
+        prev2 = prev1;
+        prev1=curr;
     }
-    cout<<dp[n];
+    cout<<prev1;
 }
