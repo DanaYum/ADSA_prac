ffe2a60f2156c4c67532f8a3df89191d34e7f5a2
Dhana <dhanachandrayumnam12@gmail.com>
2025-08-20 17:25:10 +0530

    optimal solution of house rob 1

--- DP/lc198.cpp
+++ DP/lc198.cpp
@@ -1,6 +1,9 @@
 //House Robber
 #include<bits/stdc++.h>
 using namespace std;
+
+//Memoization
+
 class Solution {
 public:
     int f(int ind,vector<int>& nums,vector<int>& dp){
@@ -17,5 +20,44 @@ public:
         int n=nums.size();
         vector<int> dp(n,-1);
         return f(n-1,nums,dp);
+    }
+};
+
+// Tabulation
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int dp[n];
+        dp[0] = nums[0];                  
+        for(int i = 1; i < n; i++){       
+            int take = nums[i];
+            if(i > 1) take += dp[i-2];
+            int notTake = dp[i-1];        
+            dp[i] = max(take, notTake);
+        }
+        return dp[n-1];
+    }
+};
+
+//Space optimazation
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2=0;                  
+        for(int i = 0; i < n; i++){       
+            int take = nums[i];
+            if(i > 1) take += prev2;
+            int notTake = 0 + prev;        
+            int curI= max(take, notTake);
+            prev2=prev;
+            prev=curI;
+        }
+        return prev;
     }
 };
