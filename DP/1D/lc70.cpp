ffbc9abdc3133f955e8f4aeddf94451c62a0d294
Dhana <dhanachandrayumnam12@gmail.com>
2025-08-27 01:00:53 +0530

    updated

---
+++ DP/1D/lc70.cpp
@@ -0,0 +1,19 @@
+//lc 70
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=3) return n;
+        int prev1=3;
+        int prev2=2;
+        int cur=0;
+        for(int i=3;i<n;i++){
+            cur=prev1+prev2;
+            prev2=prev1;
+            prev1=cur;
+        }
+        return cur;
+    }
+};
