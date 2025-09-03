f5dd8ef66420682da07c7b935628943bf3fe3c5e
Dhana <dhanachandrayumnam12@gmail.com>
2025-09-03 21:51:27 +0530

    added tabulation

--- DP/2D/lc931.cpp
+++ DP/2D/lc931.cpp
@@ -26,3 +26,34 @@ public:
         return ans;
     }
 };
+
+
+// Tabulation 
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            dp[0][j]=matrix[0][j];
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int straight=matrix[i][j] + dp[i-1][j];
+                int ld= (j>0)? matrix[i][j] + dp[i-1][j-1] : 1e9;
+                int rd= (j<m-1)? matrix[i][j] + dp[i-1][j+1] : 1e9;
+
+                dp[i][j]=min({straight,ld,rd});
+            }
+        }
+        int ans = 1e9;
+        for (int j = 0; j < m; j++) {
+            ans = min(ans, dp[n - 1][j]);
+        }
+
+        return ans;
+
+    }
+};
