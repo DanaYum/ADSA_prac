92d76457959766eebadb3fed9e9230cbeafe385c
Dhana <dhanachandrayumnam12@gmail.com>
2025-09-09 17:11:15 +0530

    longest palindromic subsequence using LCS

---
+++ DP/String/lc516.cpp
@@ -0,0 +1,25 @@
+//516. Longest Palindromic Subsequence
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int f(int ind1,int ind2, string& s, string& r,vector<vector<int>>& dp){
+        if(ind1<0 || ind2<0) return 0;
+        if(dp[ind1][ind2]!= -1) return dp[ind1][ind2];
+        if(s[ind1]==r[ind2]){
+            return 1 + f(ind1-1,ind2-1,s,r,dp);
+        }
+        return dp[ind1][ind2]=0 + max(f(ind1-1,ind2,s,r,dp),f(ind1,ind2-1,s,r,dp));
+    }
+    int longestPalindromeSubseq(string s) {
+        int n=s.size();
+        string r=s;
+        reverse(r.begin(),r.end());
+        int m=r.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,s,r,dp);
+
+    }
+};
