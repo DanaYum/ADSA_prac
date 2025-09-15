b48911ff4b8206bc85de3a557e561ece27f7097c
Dhana <dhanachandrayumnam12@gmail.com>
2025-09-15 17:44:27 +0530

    print LCS

---
+++ DP/String/print_LCS.cpp
@@ -0,0 +1,56 @@
+// memoization + printing LCS
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Recursive + memoized function for LCS length
+    int f(int i, int j, string &s, string &r, vector<vector<int>> &dp) {
+        if (i < 0 || j < 0) return 0; // base case
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == r[j])
+            return dp[i][j] = 1 + f(i - 1, j - 1, s, r, dp);
+        else
+            return dp[i][j] = max(f(i - 1, j, s, r, dp), f(i, j - 1, s, r, dp));
+    }
+
+    // Function to reconstruct the LCS string
+    string buildLCS(string &s, string &r, vector<vector<int>> &dp) {
+        int i = s.size() - 1;
+        int j = r.size() - 1;
+        int len = dp[i][j];
+        string ans(len, '$'); // placeholder string
+        int index = len - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (s[i] == r[j]) {
+                ans[index] = s[i];
+                index--;
+                i--; j--;
+            } else if (i > 0 && dp[i - 1][j] >= (j > 0 ? dp[i][j - 1] : 0)) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    string s = "abcde";
+    string r = "ace";
+    int n = s.size(), m = r.size();
+
+    // initialize dp with -1
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+
+    Solution obj;
+    int lcsLength = obj.f(n - 1, m - 1, s, r, dp);
+
+    cout << "Length of LCS = " << lcsLength << endl;
+    string ans = obj.buildLCS(s, r, dp);
+    cout << "LCS = " << ans << endl;
+    return 0;
+}
