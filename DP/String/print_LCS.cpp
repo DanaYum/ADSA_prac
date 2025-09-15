abae797be56275ebfab6c5322c8e27fed8c0e7f8
Dhana <dhanachandrayumnam12@gmail.com>
2025-09-15 17:45:25 +0530

    using tabulation

--- DP/String/print_LCS.cpp
+++ DP/String/print_LCS.cpp
@@ -54,3 +54,47 @@ int main() {
     cout << "LCS = " << ans << endl;
     return 0;
 }
+
+
+// printing LCS using tabulation
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "abcde";
+    string r = "ace";
+    int n = s.size(), m = r.size();
+
+    // Step 1: Build DP table
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i - 1] == r[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    // Step 2: Reconstruct LCS
+    int len = dp[n][m];         // length of LCS
+    string ans(len, '$');       // placeholder string
+    int i = n, j = m, index = len - 1;
+
+    while (i > 0 && j > 0) {
+        if (s[i - 1] == r[j - 1]) {
+            ans[index] = s[i - 1];
+            index--;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    cout << "LCS: " << ans << endl;
+    return 0;
+}
