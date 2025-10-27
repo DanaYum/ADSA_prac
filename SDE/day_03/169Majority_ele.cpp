445c9d0ba10713d8f2334b0b4a701205d7f1bb07
Dhana <dhanachandrayumnam12@gmail.com>
2025-10-27 16:01:53 +0530

    day3

--- SDE/day_03/169Majority_ele.cpp
+++ SDE/day_03/169Majority_ele.cpp
@@ -18,3 +18,19 @@ public:
         return 0;
     }
 };
+//using hashing
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        for(int num:nums){
+            mpp[num]++;
+        }
+        for(auto it:mpp){
+            if(it.second >nums.size()/2){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
