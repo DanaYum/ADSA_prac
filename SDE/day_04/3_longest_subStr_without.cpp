4e682842bbebccb437048b6bbac3bf1398472571
Dhana <dhanachandrayumnam12@gmail.com>
2025-11-07 14:55:26 +0530

    day4

---
+++ SDE/day_04/3_longest_subStr_without.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>    
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            vector<int> hash(256, 0);
+            for(int j = i; j < s.size(); j++) {
+                if(hash[s[j]] == 1) break;
+
+                hash[s[j]] = 1;
+                int len = j - i + 1;
+                maxlen = max(maxlen, len);
+            }
+        }
+        return maxlen;
+    }
+};
